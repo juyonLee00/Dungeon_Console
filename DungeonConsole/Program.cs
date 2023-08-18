ae98b5ffca36b0bac1b73a66869c1d9837148210
juyonLee00 <ljy012900@gmail.com>
2023-08-19 08:42:51 +0900

    #2 feat: 콘솔 글자 색 변경

--- DungeonConsole/Program.cs
+++ DungeonConsole/Program.cs
@@ -40,8 +40,12 @@ namespace DungeonConsole
             Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
             Console.WriteLine("이곳에서 전전으로 들어가기 전 활동을 할 수 있습니다.");
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("1. 상태보기");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("2. 인벤토리");
+            Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
 
