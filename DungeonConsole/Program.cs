a7294a68d2f819c3ca0a8d159a461426b6cf5ae1
juyonLee00 <ljy012900@gmail.com>
2023-08-18 13:37:59 +0900

    feat: 시작화면, 상태보기 추가

--- DungeonConsole/Program.cs
+++ DungeonConsole/Program.cs
@@ -4,9 +4,95 @@ namespace DungeonConsole
 {
     class MainClass
     {
+        private static Character player;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GameDataSetting();
+            DisplayGameIntro();
+        }
+
+        static void GameDataSetting()
+        {
+            //player info setting
+            player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+
+            //item info setting
+        }
+
+        static void DisplayGameIntro()
+        {
+            Console.Clear();
+
+            Console.WriteLine("상태보기");
+            Console.WriteLine("캐릭터의 정보 표시합니다.");
+            Console.WriteLine();
+            Console.WriteLine($"Lv.{player.Level}");
+            Console.WriteLine($"{player.Name}({player.Job})");
+            Console.WriteLine($"공격력 :{player.Atk}");
+            Console.WriteLine($"방어력 : {player.Def}");
+            Console.WriteLine($"체력 : {player.Hp}");
+            Console.WriteLine($"Gold : {player.Gold} G");
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+            int input = CheckValidInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    DisplayGameIntro();
+                    break;
+            }
+        }
+
+        static void DisplayMyInfo()
+        {
+
+        }
+
+        static void DisplayInventory()
+        {
+
+        }
+
+        static int CheckValidInput(int min, int max)
+        {
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+
+                if(parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+        }
+    }
+
+    public class Character
+    {
+        public string Name { get; }
+        public string Job { get; }
+        public int Level { get; }
+        public int Atk { get; }
+        public int Def { get; }
+        public int Hp { get; }
+        public int Gold { get; }
+
+        public Character(string name, string job, int level, int atk, int def, int hp, int gold)
+        {
+            Name = name;
+            Job = job;
+            Level = level;
+            Atk = atk;
+            Def = def;
+            Hp = hp;
+            Gold = gold;
         }
     }
 }
